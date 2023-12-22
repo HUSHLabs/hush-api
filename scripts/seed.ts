78fc6166496844c36728bc428309aaaf45e5dcce
sulejman <3107367+Sulejman@users.noreply.github.com>
2023-12-22 17:24:44 +0100

    Create seed.ts

---
+++ scripts/seed.ts
@@ -0,0 +1,29 @@
+import { PrismaService } from "../src/prisma.service";
+import { UUID } from "crypto"
+
+const prisma = new PrismaService();
+async function main() {
+  await prisma.verification.create({
+    data: {
+      id: "893a7765-773d-4b65-aebf-9f9c9644d5a5",
+      threshold: 100000,
+      contractAddress: "0xdac17f958d2ee523a2206206994597c13d831ec8",
+      callbackUrls: ["https://0xbin.xyz/verification/callback"],
+      statement: "I hereby state that i own account with more than 100k USDT at ethereum mainnet for purpose of onboarding to 0xbin at blocknumber 18842308.",
+      client: {
+        create: {
+          id: "cd79241b-f335-4462-9f4a-0424aa0e77f6",
+          name: "0xbin",
+          email: "person@0xbin.xyz",
+          logoUrl: "https://0xbin.xyz/logo.png",
+          clientSecret: "0xfefefefefefefefefefefe"
+        },
+      },
+    },
+  });
+}
+
+main().then(() => {
+    console.log("Seeding complete");
+    process.exit(0);
+})
